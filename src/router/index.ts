8a2c33c85cfab564427b6478be4b02e6e134f713
Dafne Pérez <dafneapw@gmail.com>
2026-04-21 22:34:48 +0100

    Guard de rutas según rol y según si el usuario está autenticado o no

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
+// par el guard de las rutas
+import { useAuthStore } from '@/stores/auth'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -35,6 +37,30 @@ const router = createRouter({
       ],
     },
   ],
+})
+
+router.beforeEach((to, _from) => {
+  const authStore = useAuthStore()
+
+  // si la ruta es login y ya está autenticado, redirigimos a su panel
+  if (to.name === 'login' && authStore.isAuthenticated) {
+    return authStore.isAdmin ? '/admin/events' : '/guide/events'
+  }
+
+  // si la ruta es de admin y no es admin, redirigimos
+  if (to.path.startsWith('/admin') && !authStore.isAdmin) {
+    return authStore.isAuthenticated ? '/guide/events' : '/login'
+  }
+
+  // si la ruta es de guía y no es guía, redirigimos
+  if (to.path.startsWith('/guide') && !authStore.isGuide) {
+    return authStore.isAuthenticated ? '/admin/events' : '/login'
+  }
+
+  // si no está autenticado y no es login, redirigimos
+  if (!authStore.isAuthenticated && to.name !== 'login') {
+    return '/login'
+  }
 })
 
 export default router
